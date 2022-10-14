234453f9437bf7c8758f9ebecd86fbc4d4c6822d
Erick <ericksantana.aju@gmail.com>
2022-10-14 16:44:36 -0300

    add security

---
+++ src/main/kotlin/com/forum/api/config/SecurityConfig.kt
@@ -0,0 +1,30 @@
+package com.forum.api.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.web.SecurityFilterChain
+
+@Configuration
+@EnableWebSecurity
+open class SecurityConfig {
+
+    @Bean
+    open fun filterChain(http: HttpSecurity): SecurityFilterChain {
+        http.authorizeHttpRequests()
+            .anyRequest().authenticated()
+            .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and().formLogin().disable()
+            .httpBasic()
+        return http.build()
+    }
+
+    @Bean
+    open fun encoder(): PasswordEncoder {
+        return BCryptPasswordEncoder()
+    }
+}
